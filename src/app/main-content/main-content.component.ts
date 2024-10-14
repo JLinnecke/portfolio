36ca2232010e5fd65e6a7fdc59c5533472b3e8d3
JLinnecke <jlinnecke@gmail.com>
2024-10-14 21:25:29 +0200

    add arrow animation/ first test html for about me section to check margin between sections/components

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LandingpageComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
