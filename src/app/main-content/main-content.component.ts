c27704c762d32f39602193ca75e53bcbd0abc120
JLinnecke <jlinnecke@gmail.com>
2024-10-15 15:36:46 +0200

    add my-skills component html and css/ add all images/ add animation and hover for contact <a> tag

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingpageComponent, AboutMeComponent],
+  imports: [CommonModule, LandingpageComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
