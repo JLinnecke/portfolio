b0beee2395fdb8d7a6870ce8d1496051c35724e7
JLinnecke <jlinnecke@gmail.com>
2024-10-16 06:40:19 +0200

    add portfolio component with first html, imgs and css

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingpageComponent, AboutMeComponent, MySkillsComponent],
+  imports: [CommonModule, LandingpageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
