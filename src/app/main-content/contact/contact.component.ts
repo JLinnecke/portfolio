2f94f772274bf7d8d581eb3bfe7684c3b98cb330
JLinnecke <jlinnecke@gmail.com>
2024-10-18 23:22:17 +0200

    edit contact and footer html/css and animation/ add myskills hover effect/ scroll effect add in contact and footer

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [MatInputModule],
+  imports: [MatInputModule, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
